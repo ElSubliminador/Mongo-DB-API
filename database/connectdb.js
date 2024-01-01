8bc8eaae85782ca35e63d00b0cf079b6a08544ab
Jesus Enriquez Melendez <kattvr.kidd@gmail.com>
2024-01-01 17:19:32 -0600

    Actualización de las variables de entorno
    Ahora en la Uri de MongoDB se podra conectar a una base de datos especifica para este TT

--- database/connectdb.js
+++ database/connectdb.js
@@ -1,12 +1,5 @@
 import mongoose from "mongoose";
 
-const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // Especifica el puerto deseado en las opciones de configuración
-    // por ejemplo, para el puerto 27018:
-};
-
 try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("Conectado a MongoDB Atlas");
